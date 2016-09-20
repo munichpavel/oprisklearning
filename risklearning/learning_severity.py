cd4a1f6e4c61d6a3865da0e9268fac8a4c0b6527
munichpavel <munichpavel@gmail.com>
2016-09-20 21:34:23 +0200

    Cleaned up doc string

--- risklearning/learning_severity.py
+++ risklearning/learning_severity.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-This is a skeleton file that can serve as a starting point for a Python
-console script. To run this script uncomment the following line in the
-entry_points section in setup.cfg:
-
-    console_scripts =
-     fibonacci = risklearning.skeleton:run
-
-Then run `python setup.py install` which will install the command `fibonacci`
-inside your current environment.
-Besides console scripts, the header (i.e. until _logger...) of this file can
-also be used as template for Python modules.
-
-Note: This skeleton file can be safely removed if not needed!
-
 Ideas and code snippets borrowed from 
 
 http://connor-johnson.com/2014/11/08/compound-poisson-processes/
