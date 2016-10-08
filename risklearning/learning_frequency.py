a92d3ea6e1de9136bf518cd3980370a5e1281053
munichpavel <munichpavel@gmail.com>
2016-10-08 11:34:51 +0200

    Normalize tenors
    Ensure tenors start with -1 while preserving 0 to respect nns
    distaste for big numbers
    
    [ticket: X]

--- risklearning/learning_frequency.py
+++ risklearning/learning_frequency.py
@@ -39,6 +39,9 @@ def prep_count_data(counts_df, bin_tops):
         Note that training and testing are split via negative tenor (training) 
         and non-negative tenor (testing)
     """
+#%%
+    # Normalize tenors to start at -1 and preserve 0
+    counts_df['t_ned'] = -counts_df['t']/counts_df['t'].min()    
 
     # Encode level 1 and level 2 loss categories
     le = preprocessing.LabelEncoder()
@@ -51,46 +54,44 @@ def prep_count_data(counts_df, bin_tops):
     loss_counts = pd.concat([counts_df, l_codes], axis = 1)
     
     # Prep for neural network training
-    cols_nn = ['counts', 'current_delta'] + list(range(l_codes.shape[1]))
     # Select nn-relevant columns and sort by current_deltas
-    loss_counts_nn = loss_counts[cols_nn].sort_values('current_delta')
+
+#    cols_nn = ['counts', 't'] + list(range(l_codes.shape[1]))
+    cols_nn = ['counts', 't','t_ned'] + list(range(l_codes.shape[1]))
+
+    #loss_counts_nn = loss_counts[cols_nn].sort_values('t')
+    loss_counts_nn = loss_counts[cols_nn].sort_values('t_ned')
     
-    #% Bin count data
     
-    # First create df with 
-    #n_data_tenors = loss_counts_nn['current_delta'].max() - loss_counts_nn['current_delta'].min() + 1
-    #
+    # Expand data frame for day - categories with no events
     l_codes_unique = l_codes.drop_duplicates()
     n_codes = l_codes_unique.shape[0]
     
     l_codes_unique.loc[:, 'index_new'] = range(n_codes)
     l_codes_unique = l_codes_unique.set_index('index_new')
-    
-    # Create one df block per day with all l_codes
+ 
+    # Create one df block per day with all l_codes, initialized to 0
     nn_list = [add_tenor(t, l_codes_unique) for t in 
-        range(int(loss_counts_nn['current_delta'].min()), int(loss_counts_nn['current_delta'].max()))]
+                range(int(loss_counts_nn['t'].min()), int(loss_counts_nn['t'].max()))]
 
     data_nn = pd.concat(nn_list, axis = 0)
     # Reindex to avoid duplicates
     data_nn['index_new'] = range(data_nn.shape[0])
     data_nn = data_nn.set_index('index_new')
 
-    # Bin data
-    
     # Merge with loss_counts by tenor and level 1/2 codes
-    #data_bins = pd.concat([data_nn, loss_counts_nn], axis = 1)
     left_cols = ['tenor'] + list(range(l_codes.shape[1]))
-    right_cols = ['current_delta'] + list(range(l_codes.shape[1]))
+    right_cols = ['t'] + list(range(l_codes.shape[1]))
+
      
     data_nn_bins = data_nn.merge(loss_counts_nn, left_on = left_cols, right_on = right_cols, how = 'left')
     # 'current deltas' has nans wherever no loss for given category / tenor
-    data_nn_bins = data_nn_bins.drop('current_delta',1)
+#%%    
+    data_nn_bins = data_nn_bins.drop(['t', 'tenor'],1)
     # Replace nans with 0 for counts
     data_nn_bins['counts'] = data_nn_bins['counts'].fillna(0)
-
+#%%
     #% Perform binning
-   # cts_max = data_nn_bins['counts'].max()
-    
     bin_tops_np = np.array(bin_tops)
     bin_labels = list(range(len(bin_tops_np)))
     
@@ -102,19 +103,19 @@ def prep_count_data(counts_df, bin_tops):
     data_nn_bins = data_nn_bins.drop('counts',1)
    
     #% Split into training and testing
-    data_train = data_nn_bins[data_nn_bins['tenor'] < 0]
-    data_test = data_nn_bins[data_nn_bins['tenor'] >= 0]
-    
-    x_train = data_train.drop('count_bin',1)
-    #y_train_int = data_train['count_bin']
+#    data_train = data_nn_bins[data_nn_bins['tenor'] < 0]
+#    data_test = data_nn_bins[data_nn_bins['tenor'] >= 0]
 
+    data_train = data_nn_bins[data_nn_bins['t_ned'] < 0]
+    data_test = data_nn_bins[data_nn_bins['t_ned'] >= 0]
+    
     x_train_df = data_train.drop('count_bin',1)    
-    # Encode bins with one-hot-encoding
-    #TODO check on error with simulated loss counts
+    x_test_df = data_test.drop('count_bin',1)
+  
+    # Encode bins (i.e. outputs) with one-hot-encoding
     enc = preprocessing.OneHotEncoder(sparse = False)
     y_train_df = bins2vecs(data_train['count_bin'], bin_labels, enc)
     #y_train_df = data_train['count_bin'] # No encoding, keep metric on bins
-    x_test_df = data_test.drop('count_bin',1)
     y_test_df  = bins2vecs(data_test['count_bin'], bin_labels, enc)
     #y_test_df = data_test['count_bin'] # No encoding, keep metric on bins
     #%% Convert to numpy arrays for keras / tensorflow
@@ -122,7 +123,7 @@ def prep_count_data(counts_df, bin_tops):
     y_train = y_train_df.as_matrix()
     x_test = x_test_df.as_matrix()
     y_test = y_test_df.as_matrix()
-    
+#%%    
     return([x_train, y_train, x_test, y_test])
 
 #%%
